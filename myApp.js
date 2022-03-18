1efc698daa3538605be8b4d4fd68eed144689988
Sian Mancha <slmancha20@gmail.com>
2022-03-17 21:30:50 -0500

    Use the .env File

--- myApp.js
+++ myApp.js
@@ -22,4 +22,13 @@ app.get('/json', function(req, res) {
     res.json({"message": "Hello json"})
 });
 
+// Use the .env File
+app.get('/json', function(req, res) {
+    if (process.env.MESSAGE_STYLE==='uppercase') {
+        res.json({"message" : "HELLO JSON"})
+    } else {
+        res.json({"message": "Hello json"})
+    }
+});
+
  module.exports = app;
