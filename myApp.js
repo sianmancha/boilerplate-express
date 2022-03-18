bc0e9fa968bad6c82a6f383292036e4e26dacb4f
Sian Mancha <slmancha20@gmail.com>
2022-03-17 20:51:00 -0500

    Start a Working Express Server

--- myApp.js
+++ myApp.js
@@ -4,6 +4,11 @@ var app = express();
 // Meet the Node console
 console.log("Hello World")
 
+// Start a Working Express Server
+app.get('/', function(req, res) {
+    res.send("Hello Express")
+});
+
 
 
 
