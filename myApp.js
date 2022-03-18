a7b3b1cb6a1c7bff0e05883b784e567fc2609b4d
Sian Mancha <slmancha20@gmail.com>
2022-03-17 21:41:15 -0500

    edited if/else statement for .env section

--- myApp.js
+++ myApp.js
@@ -26,9 +26,9 @@ app.get('/json', function(req, res) {
 // Use the .env File
 app.get('/json', function(req, res) {
     if (process.env.MESSAGE_STYLE==='uppercase') {
-        res.json({"message" : "HELLO JSON"})
+        return res.json({"message" : "HELLO JSON"})
     } else {
-        res.json({"message": "Hello json"})
+        return res.json({"message": "Hello json"})
     }
 });
 
